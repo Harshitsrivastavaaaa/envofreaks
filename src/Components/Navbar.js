6eb9453c405758d7af4db1e97b6ef7cf20e211fb
Harshit Srivastava <Harshitsrivastava0914@gmail.com>
2025-08-29 15:17:43 +0530

    C2.2

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <><nav className="navStyle">
             {/* <Link to={"/"} className="LS"><img src={require('../Assets/logo.png')} alt="Pentonic Logo" height="40px" width="40px" /></Link> */}
-            <Link to={"/about"} className="LS">About</Link>
             <Link to={"/"} className="LS">Home</Link>
             <Link to={"/envofreaks"} className="LS">Home</Link>
+            <Link to={"/about"} className="LS">About</Link>
             <Link to={"/contact"} className="LS">Contact</Link>
         </nav>
         <nav className="navStyle1">
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+ <React.StrictMode>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/envofreaks' element={<Home/>} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      {/* <Route path='/Signup' element={<Signup />} /> */}
+      {/* <Route path='/' element={<img src={logo} alt="EnvoFreaks Logo" height="500px" width="350px" />} /> */}
+
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
